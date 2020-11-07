f9208471e07e0e5687048cf084d889ba9b2f38ae
Derek Wong <djwong23@gmail.com>
2020-11-07 16:38:48 -0500

    added man

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import man from './man.svg';
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{alignItems: "center"}}>
+      <p style={{textAlign: "center"}}>Select which area you'd like like to exercise:</p>
+      <img className = "photo" src={man}/>
     </div>
-  );
+  )
+  
 }
 
 export default App;
