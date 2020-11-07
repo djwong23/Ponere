28e565f86e15bb24f77746239a22d9e07355f4b4
Derek Wong <djwong23@gmail.com>
2020-11-07 17:45:11 -0500

    made prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import man from './man.svg';
+import logo from "./logo.svg";
+import "./App.css";
+import man from "./man.svg";
 function App() {
-  
   return (
-    <div style={{alignItems: "center"}}>
-      <p style={{textAlign: "center"}}>Select which area you'd like like to exercise:</p>
-      <img className = "photo" src={man}/>
+    <div>
+      <div className="banner">
+        <p style={{ textAlign: "center" }}>Ponere</p>
+      </div>
+      <div style={{ alignItems: "center" }}>
+        <p style={{ textAlign: "center", fontSize: "20px" }}>
+          Select which area you'd like to correct:
+        </p>
+        <img className="photo" src={man} />
+      </div>
     </div>
-  )
-  
+  );
 }
 
 export default App;
