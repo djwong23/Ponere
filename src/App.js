76fed245cb102dfb7e132e4a888f180ac77dbbd8
David Wang <davidwangmo@gmail.com>
2020-11-07 21:58:53 -0600

    changed popover

--- src/App.js
+++ src/App.js
@@ -2,45 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import man from "./man.svg";
 import React, { useState } from 'react';
-import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { render } from "@testing-library/react";
 
 function App() {
-  const [popoverOpen, setPopover] = useState(0);
+    return (
+      <div>
+        <div className="banner">
+          <a style={{ textAlign: "center", textDecoration: "none", color: '#535353', paddingLeft: '10%', position: 'absolute', top: '33%'}} href = "/">Ponere</a>
+        </div>
+        <div style={{ alignItems: "center" }} >
+          <p style={{ textAlign: "center", fontSize: "20px" }}>
+            Select which area you'd like to correct:
+          </p>
+          <img className="photo" src={man}/>
+          <Button className="neck" id="Popover1" type="button">
+          </Button>
+          <UncontrolledPopover trigger="focus" placement="right" target="Popover1">
+            <PopoverHeader>The Neck</PopoverHeader>
+            <PopoverBody>Temporary</PopoverBody>
+          </UncontrolledPopover>
 
-  const toggle = (id) => setPopover(id)
-  return (
-    <div>
-      <div className="banner">
-        <a style={{ textAlign: "center", textDecoration: "none", color: '#535353', paddingLeft: '10%', position: 'absolute', top: '33%'}} href = "/">Ponere</a>
+          <Button className="upperback" id="Popover2" type="button">
+          </Button>
+          <UncontrolledPopover trigger="legacy" placement="right" target="Popover2">
+            <PopoverHeader>Upper Back</PopoverHeader>
+            <PopoverBody>Temporary</PopoverBody>
+          </UncontrolledPopover>
+
+          <Button className="lowerback" id="Popover3" type="button">
+          </Button>
+          <UncontrolledPopover trigger="legacy" placement="right" target="Popover3">
+            <PopoverHeader>Lower Back</PopoverHeader>
+            <PopoverBody>Temporary</PopoverBody>
+          </UncontrolledPopover>
+        </div>
       </div>
-      <div style={{ alignItems: "center" }} >
-        <p style={{ textAlign: "center", fontSize: "20px" }}>
-          Select which area you'd like to correct:
-        </p>
-        <img className="photo" src={man}/>
-        <Button className="neck" id="Popover1" type="button" onClick={() => toggle(1)}>
-        </Button>
-        <Popover className="popup" placement="right" isOpen={popoverOpen === 1} target="Popover1">
-          <PopoverHeader>The Neck</PopoverHeader>
-          <PopoverBody>Temporary</PopoverBody>
-        </Popover>
-
-        <Button className="upperback" id="Popover2" type="button" onClick={() => toggle(2)}>
-        </Button>
-        <Popover className="popup" placement="right" isOpen={popoverOpen === 2} target="Popover2">
-          <PopoverHeader>Upper Back</PopoverHeader>
-          <PopoverBody>Temporary</PopoverBody>
-        </Popover>
-
-        <Button className="lowerback" id="Popover3" type="button" onClick={() => toggle(3)}>
-        </Button>
-        <Popover className="popup" placement="right" isOpen={popoverOpen === 3} target="Popover3">
-          <PopoverHeader>Lower Back</PopoverHeader>
-          <PopoverBody>Temporary</PopoverBody>
-        </Popover>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
