868b8c7308fff6a6e8abb3fc9ab942fa52427272
Derek Wong <djwong23@gmail.com>
2020-11-08 02:33:13 -0500

    added bottom text, changed colors of hover, converted to class

--- src/App.js
+++ src/App.js
@@ -8,87 +8,101 @@ import lowerback2 from "./back-stretches2.jpeg";
 import neck from "./neck-stretches.jpg";
 import ReactTooltip from "react-tooltip";
 import { UncontrolledPopover, PopoverHeader, PopoverBody } from "reactstrap";
+import React from 'react';
 
-function App() {
-  let picturesOpen = " ";
-  function setToOpen(string) {
-    if (picturesOpen != " ") {
-      document.getElementById(picturesOpen).style.opacity = 0;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      picturesOpen: " ",
+    };
+  }
+
+  setToOpen(string) {
+    this.setState({clicked: true});
+    if (this.state.picturesOpen != " ") {
+      document.getElementById(this.state.picturesOpen).style.opacity = 0;
     }
-    picturesOpen = string;
+    this.state.picturesOpen = string;
     document.getElementById(string).style.opacity = 1;
   }
 
-  return (
-    <div>
-      <div className="banner">
-        <a className="home" href="/">
-          Ponere
-        </a>
-      </div>
-      <div className="bannerFiller"></div>
-      <h1 style={{ textAlign: "center", fontSize: "20px" }}>
-        Select which area you'd like to correct:
-      </h1>
-
-      <div className="imageHolder">
-        <img
-          className="neck fade images"
-          id="neck"
-          width="30%"
-          height="auto"
-          src={neck}
-          alt="neck"
-        />
-        <div className="upper fade" id="upper">
-          <img
-            className="upper1 images"
-            width="30%"
-            height="auto"
-            src={upperback1}
-            alt="back"
-          />
-          <img
-            className="upper2 images"
-            width="30%"
-            height="auto"
-            src={upperback2}
-            alt="back"
-          />
+  render() {
+    return (
+      <div>
+        <div className="banner">
+          <a className="home" href="/">
+            Ponere
+          </a>
         </div>
-        <div className="lower fade" id="lower">
-          <img
-            className="lower1 images"
-            width="30%"
-            height="auto"
-            src={lowerback1}
-            alt="lowerback1"
-          />
-          <img
-            className="lower2 images"
-            width="30%"
-            height="auto"
-            src={lowerback2}
-            alt="lowerback2"
-          />
-        </div>
+        <div className="bannerFiller"></div>
+        <h1 style={{ textAlign: "center", fontSize: "20px" }}>
+          Select which area you'd like to stretch:
+        </h1>
 
-        <img className="photo" src={man} alt="body for selection" />
-        <a data-tip="Neck" id="neckDiv" onClick={() => setToOpen("neck")}></a>
-        <a
-          data-tip="Upper Back & Shoulders"
-          id="upperBackDiv"
-          onClick={() => setToOpen("upper")}
-        ></a>
-        <a
-          data-tip="Lower Back & Hip Flexors"
-          id="lowerBackDiv"
-          onClick={() => setToOpen("lower")}
-        ></a>
-        <ReactTooltip />
+        <div className="imageHolder">
+          <img
+            className="neck fade images"
+            id="neck"
+            width="30%"
+            height="auto"
+            src={neck}
+            alt="neck"
+          />
+          <div className="upper fade" id="upper">
+            <img
+              className="upper1 images"
+              width="30%"
+              height="auto"
+              src={upperback1}
+              alt="back"
+            />
+            <img
+              className="upper2 images"
+              width="30%"
+              height="auto"
+              src={upperback2}
+              alt="back"
+            />
+          </div>
+          <div className="lower fade" id="lower">
+            <img
+              className="lower1 images"
+              width="30%"
+              height="auto"
+              src={lowerback1}
+              alt="lowerback1"
+            />
+            <img
+              className="lower2 images"
+              width="30%"
+              height="auto"
+              src={lowerback2}
+              alt="lowerback2"
+            />
+          </div>
+
+          <img className="photo" src={man} alt="body for selection" />
+          <a data-tip="Neck" id="neckDiv" onClick={() => this.setToOpen("neck")}></a>
+          <a
+            data-tip="Upper Back & Shoulders"
+            id="upperBackDiv"
+            onClick={() => this.setToOpen("upper")}
+          ></a>
+          <a
+            data-tip="Lower Back & Hip Flexors"
+            id="lowerBackDiv"
+            onClick={() => this.setToOpen("lower")}
+          ></a>
+          <ReactTooltip />
+        </div>
+        
+        <div className="instruction">{this.state.clicked == true ? <p>Here are some exercises to alleviate bad posture.</p> : null}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
