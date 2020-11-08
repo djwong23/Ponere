4bc4e95255a58abe9ee3f53fd81575570e0bf464
Derek Wong <djwong23@gmail.com>
2020-11-07 23:41:38 -0500

    formatted using prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import man from "./mans.png";
-import ReactTooltip from 'react-tooltip';
-import { UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
+import ReactTooltip from "react-tooltip";
+import { UncontrolledPopover, PopoverHeader, PopoverBody } from "reactstrap";
 function App() {
   return (
     <div>
@@ -13,27 +13,35 @@ function App() {
       </div>
       <div className="bannerFiller"></div>
       <h1 style={{ textAlign: "center", fontSize: "20px" }}>
-              Select which area you'd like to correct:
+        Select which area you'd like to correct:
       </h1>
       <div className="imageHolder">
-          <img className="photo" src={man} alt = "body for selection"/>
-            <a data-tip = "Neck" id="neckDiv"></a>
-            <a data-tip = "Upper Back & Shoulders" id="upperBackDiv"></a>
-            <a data-tip = "Lower Back & Hip Flexors" id="lowerBackDiv"></a>
-            <ReactTooltip/>
+        <img className="photo" src={man} alt="body for selection" />
+        <a data-tip="Neck" id="neckDiv"></a>
+        <a data-tip="Upper Back & Shoulders" id="upperBackDiv"></a>
+        <a data-tip="Lower Back & Hip Flexors" id="lowerBackDiv"></a>
+        <ReactTooltip />
       </div>
-          <UncontrolledPopover trigger="legacy" placement="left" target="neckDiv">
-            <PopoverHeader>The Neck</PopoverHeader>
-            <PopoverBody>Temporary</PopoverBody>
-          </UncontrolledPopover>
-          <UncontrolledPopover trigger="legacy" placement="right" target="upperBackDiv">
-            <PopoverHeader>Upper Back</PopoverHeader>
-            <PopoverBody>Temporary</PopoverBody>
-          </UncontrolledPopover>
-          <UncontrolledPopover trigger="legacy" placement="left" target="lowerBackDiv">
-            <PopoverHeader>Lower Back</PopoverHeader>
-            <PopoverBody>Temporary</PopoverBody>
-          </UncontrolledPopover>
+      <UncontrolledPopover trigger="legacy" placement="left" target="neckDiv">
+        <PopoverHeader>The Neck</PopoverHeader>
+        <PopoverBody>Temporary</PopoverBody>
+      </UncontrolledPopover>
+      <UncontrolledPopover
+        trigger="legacy"
+        placement="right"
+        target="upperBackDiv"
+      >
+        <PopoverHeader>Upper Back</PopoverHeader>
+        <PopoverBody>Temporary</PopoverBody>
+      </UncontrolledPopover>
+      <UncontrolledPopover
+        trigger="legacy"
+        placement="left"
+        target="lowerBackDiv"
+      >
+        <PopoverHeader>Lower Back</PopoverHeader>
+        <PopoverBody>Temporary</PopoverBody>
+      </UncontrolledPopover>
     </div>
   );
 }
