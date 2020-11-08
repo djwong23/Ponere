f5b69b466642ab3ad39a77aae3ea870580509941
Derek Wong <djwong23@gmail.com>
2020-11-07 19:43:28 -0500

    fixed padding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import man from "./man.svg";
 function App() {
   return (
     <div>
       <div className="banner">
-        <a className ="home" href = "/">Ponere</a>
+        <a className="home" href="/">
+          Ponere
+        </a>
       </div>
       <div style={{ alignItems: "center" }}>
         <p style={{ textAlign: "center", fontSize: "20px" }}>
           Select which area you'd like to correct:
         </p>
-        <img className="photo" src={man} />
+        <img className="photo" src={man} alt = "body for selection"/>
       </div>
     </div>
   );
