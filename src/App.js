bd82d3e207a6b5b003be9da402f39645481d4d08
RahulNTrivedi <rahult0705@gmail.com>
2020-11-08 01:25:04 -0500

    images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import man from "./mans.png";
+import upperback1 from "./scapular-wall-slide.jpg";
+import upperback2 from "./upperRope.png";
+import lowerback1 from "./back-stretches1.jpeg";
+import lowerback2 from "./back-stretches2.jpeg";
+import neck from "./neck-stretches.jpg"
 import ReactTooltip from 'react-tooltip';
 import { UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
+
+
+
 function App() {
+  let picturesOpen = " ";
+  function setToOpen(string) {
+    if(picturesOpen != " "){
+      document.getElementById(picturesOpen).style.opacity = 0;
+    }
+    picturesOpen = string;
+    document.getElementById(string).style.opacity = 1;
+  }
+
   return (
     <div>
       <div className="banner">
@@ -15,25 +32,25 @@ function App() {
       <h1 style={{ textAlign: "center", fontSize: "20px" }}>
               Select which area you'd like to correct:
       </h1>
+
       <div className="imageHolder">
-          <img className="photo" src={man} alt = "body for selection"/>
-            <a data-tip = "Neck" id="neckDiv"></a>
-            <a data-tip = "Upper Back & Shoulders" id="upperBackDiv"></a>
-            <a data-tip = "Lower Back & Hip Flexors" id="lowerBackDiv"></a>
+            <img className="neck" id="neck" width="30%" height="auto" src = {neck} alt="neck"/>
+            <div className="upper" id="upper">
+              <img className="upper1" width="30%" height="auto" src = {upperback1} alt="back"/>
+              <img className="upper2" width="30%" height="auto" src = {upperback2} alt="back"/>
+            </div>
+            <div className="lower" id="lower">
+              <img className="lower1"  width="30%" height="auto" src = {lowerback1} alt="lowerback1"/>
+              <img className="lower2"  width="30%" height="auto" src = {lowerback2} alt="lowerback2"/>
+            </div>
+
+            <img className="photo" src={man} alt = "body for selection"/>
+            <a data-tip = "Neck" id="neckDiv" onClick={() => setToOpen("neck")}></a>
+            <a data-tip = "Upper Back & Shoulders" id="upperBackDiv" onClick={() => setToOpen("upper")}></a>
+            <a data-tip = "Lower Back & Hip Flexors" id="lowerBackDiv" onClick={() => setToOpen("lower")}></a>
             <ReactTooltip/>
       </div>
-          <UncontrolledPopover trigger="legacy" placement="left" target="neckDiv">
-            <PopoverHeader>The Neck</PopoverHeader>
-            <PopoverBody>Temporary</PopoverBody>
-          </UncontrolledPopover>
-          <UncontrolledPopover trigger="legacy" placement="right" target="upperBackDiv">
-            <PopoverHeader>Upper Back</PopoverHeader>
-            <PopoverBody>Temporary</PopoverBody>
-          </UncontrolledPopover>
-          <UncontrolledPopover trigger="legacy" placement="left" target="lowerBackDiv">
-            <PopoverHeader>Lower Back</PopoverHeader>
-            <PopoverBody>Temporary</PopoverBody>
-          </UncontrolledPopover>
+
     </div>
   );
 }
